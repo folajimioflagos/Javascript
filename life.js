00d25a1bd11afb5274d723b3823cd0e80c73b351
folajimioflagos <folarh.j@gmail.com>
2022-06-28 15:25:51 -0700

    some changes in code structure were made

--- life.js
+++ life.js
@@ -1,2 +1,44 @@
-// let Name = 'Name: Ayodele'
-alert(`Welcome to my first javascript`);
+
+let num1 = prompt("Enter a number");
+let operator = prompt("Enter + - * /");
+let num2 = prompt("Enter another number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let result;
+
+if (operator == "+") {
+    result = num1 + num2;
+}
+
+else if (operator == "-") {
+    result = num1 - num2;
+}
+
+else if (operator == "*") {
+    result = num1 * num2;
+}
+
+else if (operator == "/") {
+    result = num1 / num2;
+}
+
+alert(result);
+
+
+// let addResult = num1 + num2;
+// let subResult = num1 - num2;
+// let multiResult = num1 * num2;
+// let divResult = num1 / num2;
+
+
+// alert("The sum of " + num1 + " and " + num2 + " is: " + addResult);
+
+// alert("The difference of " + num1 + "and " + num2 + " is: " + subResult);
+
+// alert("The product of " + num1 + "and " + num2 + " is: " + multiResult);
+
+// alert("The ratio of " + num1 + "and " + num2 + " is: " + divResult);
+
+
